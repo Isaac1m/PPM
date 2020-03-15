2b08365a812d125f5208ff0cbfb4d859545c681f
Isaac Jandalala <isaacjandalala@gmail.com>
2020-03-15 13:43:37 +0200

    Initial project scaffold

---
+++ src/test/java/io/isaacj/projectmgmt/ProjectmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.isaacj.projectmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
