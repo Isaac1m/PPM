ecca2d67ca74fafa8da3aaed9382b474315bca27
Isaac Jandalala <isaacjandalala@gmail.com>
2020-03-15 15:02:12 +0200

    Added validation rules for Project object

--- src/main/java/io/isaacj/projectmgmt/domain/Project.java
+++ src/main/java/io/isaacj/projectmgmt/domain/Project.java
@@ -1,6 +1,10 @@
 package io.isaacj.projectmgmt.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,14 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String name;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "The project size should be 4-5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     public Project() {
