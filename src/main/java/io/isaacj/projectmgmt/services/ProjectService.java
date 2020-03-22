5f3ce513e1e44a7cbbd14f66a4344473af24acb3
Isaac Jandalala <isaacjandalala@gmail.com>
2020-03-22 20:12:21 +0200

    Created custom exception handler for duplicate project IDs.

--- src/main/java/io/isaacj/projectmgmt/services/ProjectService.java
+++ src/main/java/io/isaacj/projectmgmt/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.isaacj.projectmgmt.services;
 
 import io.isaacj.projectmgmt.domain.Project;
+import io.isaacj.projectmgmt.exceptions.ProjectIdException;
 import io.isaacj.projectmgmt.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class ProjectService {
     ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toLowerCase());
+            return projectRepository.save(project);
+        } catch(Exception e) {
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exits");
+        }
     }
 }
